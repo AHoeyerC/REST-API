6fd34c87444b40e3945556a7a502f80566c57d28
AHoeyer <super-hoyer@hotmail.com>
2019-10-22 00:58:37 +0200

    as

--- api/routes/studentlesson.js
+++ api/routes/studentlesson.js
@@ -3,13 +3,28 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const StudentLesson = require('../models/studentLesson');
+const Lesson = require('../models/lesson')
 
 router.get('/', (req, res, next) => {
     StudentLesson
     .find()
+    .select('lesson quantity _id')
     .exec()
     .then(docs => {
-        res.status(200).json(docs);
+        res.status(200).json({
+            count: docs.length,
+            studentLesson: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    lesson: doc.lesson,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: "http://localhost:3000/studentlesson/" + doc._id
+                    }
+                }
+            })
+        });
     })
     .catch(err => {
         res.status(500).json({
@@ -20,41 +35,106 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const studentLesson = new StudentLesson({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        lesson: req.body.lessonId
+    Lesson.findById(req.body.lessonId)
+        .then(lesson => {
+            if (!lesson) {
+                return res.status(404).json({
+                    message: 'Lesson not found'
+                });
+            }
+            const studentLesson = new StudentLesson({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                lesson: req.body.lessonId
+            });
+            return studentLesson
+            .save()
+        })
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Student Lesson Stored',
+                createdStudentlesson: {
+                    _id: result._id,
+                    lesson: result.lesson,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: "http://localhost:3000/studentlesson/" + result._id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+});
+
+router.get('/:studentLessonId', (req, res, next) => {
+    StudentLesson.findById(req.params.studentlessonId)
+    .exec()
+    .then(studentLesson => {
+        res.status(200).json({
+            studentLesson: studentLesson,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/studentlesson'
+            }
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
-    studentLesson
-    .save()
+    
+});
+
+router.patch('/:studentlessonId', (req, res, next) => {
+    const id = req.params.studentlessonId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    StudentLesson.update({_id: id}, { $set: updateOps })
+    .exec()
     .then(result => {
-        console.log(result);
-        res.status(201).json(result);
+        res.status(200).json({
+            message: 'Student Lesson updated',
+            request: {
+                type: 'GET',
+                url: "http://localhost:3000/studentlesson/" + id
+            }
+        });
     })
     .catch(err => {
         console.log(err);
         res.status(500).json({
             error: err
-        })
-    });
-});
-
-router.get('/:studentLessonId', (req, res, next) => {
-    const id = req.params.studentLessonId;
-    
-});
-
-router.get('/:studentlessonId', (req, res, next) => {
-    res.status(201).json({
-        message: 'Student lesson details',
-        studentlessonId: req.params.studentlessonId
+        });
     });
 });
 
 router.delete('/:studentlessonId', (req, res, next) => {
-    res.status(201).json({
-        message: 'Student lesson deleted',
-        studentlessonId: req.params.studentlessonId
+    StudentLesson.remove({ _id: req.params.studentlessonId})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Student Lesson deleted',
+            request: {
+                type: "POST",
+                url: "http://localhost:3000/studentlesson",
+                body: { lessonId: 'ID', quantity: 'Number'}
+            }
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
