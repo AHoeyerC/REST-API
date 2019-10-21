01e8bdca793d9550d6059312779ddc85abfa631a
AHoeyer <super-hoyer@hotmail.com>
2019-10-21 22:41:33 +0200

    Patch

--- api/models/msgBoard.js
+++ api/models/msgBoard.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const msgBoardSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    message: String,
+    messageB: String,
 });
 
 module.exports = mongoose.model('MsgBoard', msgBoardSchema);
--- api/routes/msgboard.js
+++ api/routes/msgboard.js
@@ -5,8 +5,17 @@ const mongoose = require('mongoose');
 const MsgBoard = require('../models/msgBoard');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling get'
+    MsgBoard.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
@@ -14,15 +23,15 @@ router.post('/', (req, res, next) => {
     const msg = new MsgBoard({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        message: req.body.message,
+        messageB: req.body.messageB,
     });
     msg
     .save()
     .then(result => {
         console.log(result);
         res.status(201).json({
-            message: 'Handling post',
-            createdMessage: result
+            messageB: 'Handling post',
+            createdMessageB: result
         });
     })
     .catch(err => {
@@ -39,7 +48,11 @@ router.get('/:msgId', (req, res, next) => {
     .exec()
     .then(doc => {
         console.log(doc);
-        res.status(200).json(doc);
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({message: "No valid entry found for provided ID"});
+        }
     })
     .catch(err => {
         console.log(err);
@@ -47,14 +60,26 @@ router.get('/:msgId', (req, res, next) => {
     });
 });
 router.patch('/:msgId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated message'
-    });
+    const id = req.params.msgId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    MsgBoard.update({_id: id}, { $set: updateOps });
 });
 
 router.delete('/:msgId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted message'
+    const id = req.params.msgId;
+    MsgBoard.remove({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
